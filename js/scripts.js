d0e57dc45cffe2a91054838bf48dd77de45b1925
SANDRO00O <mk63cp@gmail.com>
2025-06-05 22:50:11 +0300

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,15 @@
 var phraseCarouselInterval;
+let topZIndex = 100; // لرفع كل نافذة جديدة للأعلى
 
 (function($, root, undefined) {
     $(function() {
         'use strict';
         startTime();
         
-        // معالجة حدث النقر على زر الإغلاق
         $(document).on('click', '.btn-close', function() {
             const win = $(this).closest('.window');
-            
-            // إخفاء النافذة
             win.toggleClass('hidden');
-            
-            // إعادة تعيين الموقع (top, left, transform) إلى الافتراضي
-            win.css({
-                top: '',
-                left: '',
-                transform: ''
-            });
-            
-            // إذا كانت نافذة الاشتراك مخفية، أوقف الكاروسيل وأعد موضع العبارات
+            win.css({ top: '', left: '', transform: '', zIndex: '' });
             if ($(".subscribe-window").hasClass("hidden")) {
                 clearInterval(phraseCarouselInterval);
                 phraseCarouselInterval = null;
@@ -27,21 +17,10 @@ var phraseCarouselInterval;
             }
         });
         
-        // معالجة حدث اللمس على زر الإغلاق (للجوال)
         $(document).on('touchend', '.btn-close', function() {
             const win = $(this).closest('.window');
-            
-            // إخفاء النافذة
             win.toggleClass('hidden');
-            
-            // إعادة تعيين الموقع (top, left, transform) إلى الافتراضي
-            win.css({
-                top: '',
-                left: '',
-                transform: ''
-            });
-            
-            // إذا كانت نافذة الاشتراك مخفية، أوقف الكاروسيل وأعد موضع العبارات
+            win.css({ top: '', left: '', transform: '', zIndex: '' });
             if ($(".subscribe-window").hasClass("hidden")) {
                 clearInterval(phraseCarouselInterval);
                 phraseCarouselInterval = null;
@@ -54,11 +33,11 @@ var phraseCarouselInterval;
         });
         
         $(document).on('click', '.nav-copyright', function() {
-            $('.copyright-window').toggleClass('hidden').addClass('front');
+            openWindow('copyright-window');
         });
         
         $(document).on('click', '.time-toggle', function() {
-            $('.world-time-container').toggleClass('hidden');
+            openWindow('world-time-container');
         });
         
         $(document).on('click', '.like-this-page', function() {
@@ -69,22 +48,33 @@ var phraseCarouselInterval;
         });
         
         $(document).on('click', '.win-trigger', function() {
-            openWindow($(this).attr('data-trigger'));
-            if ($(this).attr('data-trigger') == "subscribe-window" && !phraseCarouselInterval) {
-                subscribePhraseCarousel(document.querySelector(".signup-for-answers-answers"), document.querySelector(".scrolling-phrase-items"));
+            const target = $(this).attr('data-trigger');
+            openWindow(target);
+            if (target == "subscribe-window" && !phraseCarouselInterval) {
+                subscribePhraseCarousel(
+                    document.querySelector(".signup-for-answers-answers"),
+                    document.querySelector(".scrolling-phrase-items")
+                );
             }
         });
         
         $(document).on('touchend', '.win-trigger', function() {
-            openWindow($(this).attr('data-trigger'));
-            if ($(this).attr('data-trigger') == "subscribe-window" && !phraseCarouselInterval) {
-                subscribePhraseCarousel(document.querySelector(".signup-for-answers-answers"), document.querySelector(".scrolling-phrase-items"));
+            const target = $(this).attr('data-trigger');
+            openWindow(target);
+            if (target == "subscribe-window" && !phraseCarouselInterval) {
+                subscribePhraseCarousel(
+                    document.querySelector(".signup-for-answers-answers"),
+                    document.querySelector(".scrolling-phrase-items")
+                );
             }
         });
         
+        // إذا تم النقر على نافذة غير أمامية، اجعلها في الأمام
         $(document).on('click', '.window:not(.front)', function() {
             $('.window').removeClass('front');
             $(this).addClass('front');
+            topZIndex++;
+            $(this).css('z-index', topZIndex);
         });
         
         // Clock
@@ -106,7 +96,6 @@ const fonts2 = [
     'Tiny5', 'SixtyfourConvergence', 'PressStart2P',
     'PixelifySans', 'VariableFont_ROND', 'CoralPixels', 'DOSANK24'
 ];
-
 let isPlaying2 = false;
 
 async function loadFonts() {
@@ -118,7 +107,6 @@ async function playLoop2() {
     isPlaying2 = true;
     await loadFonts();
     const text = document.getElementById("changing-text");
-    
     let i = 0;
     const interval = setInterval(() => {
         text.style.fontFamily = fonts2[i % fonts2.length];
@@ -135,14 +123,17 @@ function enterSite() {
     playLoop2();
 }
 
-// فتح نافذة بدون إخفاء غيرها
+// ✅ فتح نافذة مع رفعها للأمام
 function openWindow(windowAttr) {
-    $('.' + windowAttr).removeClass('hidden').addClass('front');
+    const win = $('.' + windowAttr);
+    $('.window').removeClass('front');
+    win.removeClass('hidden').addClass('front');
+    topZIndex++;
+    win.css('z-index', topZIndex);
 }
 
 // السنة الحالية
 const year = new Date().getFullYear();
-
 for (const el of document.getElementsByClassName("current-year")) {
     el.innerHTML = year;
 }
@@ -180,15 +171,12 @@ function subscribePhraseCarousel(wrapper, items) {
     
     items.appendChild(cloneFirst);
     items.insertBefore(cloneLast, firstSlide);
-    
     items.addEventListener('transitionend', checkIndex);
     
     function shiftSlide(dir, action) {
         items.classList.add('shifting');
-        
         if (allowShift) {
             if (!action) { posInitial = items.offsetTop; }
-            
             if (dir == 1) {
                 items.style.top = (posInitial - slideSize) + "px";
                 index++;
@@ -197,23 +185,19 @@ function subscribePhraseCarousel(wrapper, items) {
                 index--;
             }
         };
-        
         allowShift = false;
     }
     
     function checkIndex() {
         items.classList.remove('shifting');
-        
         if (index == -1) {
             items.style.top = -(slidesLength * slideSize) + "px";
             index = slidesLength - 1;
         }
-        
         if (index == slidesLength) {
             items.style.top = -(1 * slideSize) + "px";
             index = 0;
         }
-        
         allowShift = true;
     }
     
